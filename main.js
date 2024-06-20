c91e3460bd9195e1d043ad7d173ba5396040750f
Дарья <dforst09@gmail.com>
2024-06-20 11:43:50 +0300

    complete

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ import headerNav from './src/js/header-nav';
 import { accordions } from './src/js/accordion';
 
 document.addEventListener('DOMContentLoaded', () => {
-    //header menu accordions
-    const headerAccordions = document.querySelectorAll(".menu__item.dropdown>.menu__link");
-    accordions(headerAccordions, 0);
+    if (window.innerWidth <= 1870) {
+        //header menu accordions
+        const headerAccordions = document.querySelectorAll(".menu__item.dropdown>.menu__link");
+        accordions(headerAccordions, 0);
+    }
 
     //header menu 
     const menuBtn = document.querySelector('.header__btn');
--- src/js/header.js
+++ src/js/header.js
@@ -7,26 +7,26 @@ function headerMenu(menuBtn) {
             this.classList.toggle('active');
             this.closest('.header').classList.toggle('burger-open');
             if (this.classList.contains('active')) {
-                if (window.innerWidth > 1024) {
-                    console.log(bodyScrollBar);
-                    bodyScrollBar.updatePluginOptions('modal', { open: true });
-                    document.querySelectorAll('.scrollbar-track').forEach(item => { item.classList.remove('show') });
-                    document.querySelector('.header:not(.header-nav)').style.zIndex = '23';
-                }
-                else {
-                    openPopup();
-                }
+                // if (window.innerWidth > 1024) {
+                //     console.log(bodyScrollBar);
+                //     bodyScrollBar.updatePluginOptions('modal', { open: true });
+                //     document.querySelectorAll('.scrollbar-track').forEach(item => { item.classList.remove('show') });
+                //     document.querySelector('.header:not(.header-nav)').style.zIndex = '23';
+                // }
+                // else {
+                openPopup();
+                // }
                 document.querySelector('.header__menu-wrapper').classList.add('active');
             }
             else {
-                if (window.innerWidth > 1024) {
-                    bodyScrollBar.updatePluginOptions('modal', { open: false });
-                    document.querySelectorAll('.scrollbar-track').forEach(item => { item.classList.add('show') });
-                    document.querySelector('.header:not(.header-nav)').style.zIndex = '';
-                }
-                else {
-                    closePopup();
-                }
+                // if (window.innerWidth > 1024) {
+                //     bodyScrollBar.updatePluginOptions('modal', { open: false });
+                //     document.querySelectorAll('.scrollbar-track').forEach(item => { item.classList.add('show') });
+                //     document.querySelector('.header:not(.header-nav)').style.zIndex = '';
+                // }
+                // else {
+                closePopup();
+                // }
                 document.querySelector('.header__menu-wrapper').classList.remove('active');
             }
         });
